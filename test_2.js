9df32e0dcfb80d02f26671ec4938d1e1ae8f9c5b
Nintendddo <98233690+Nintendddo@users.noreply.github.com>
2022-10-05 00:07:53 +0300

    lesson_2

---
+++ test_2.js
@@ -0,0 +1,13 @@
+let keys = document.querySelectorAll('.key');
+let display = document.querySelector('.display');
+let clear = document.querySelector('.clear');
+
+for (let key of keys) {
+  key.onclick = function () {
+    display.textContent += key.textContent
+  };
+};
+
+clear.onclick = function () {
+ display.textContent = ' ' 
+};
