fbfea27ddaff0a4cc45daf0abbfd7b45308bd298
rocetDim <dmitriydem81@mail.ru>
2022-10-11 23:50:01 +0300

    JS основы

---
+++ lesson_5.js
@@ -0,0 +1,6 @@
+let timeInMinutes = 180;
+console.log(timeInMinutes / 10);
+let jumps = (timeInMinutes / 10 * 3);
+console.log(jumps);
+let message = 'За ' + timeInMinutes + ' минут в соцсетях полагается ' + jumps + ' прыжков.';
+console.log(message);
