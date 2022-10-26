4641ea6e5f8ffd2f12f0be1117e03af6277e704a
rocetDim <dmitriydem81@mail.ru>
2022-10-26 22:07:32 +0300

    Рефакторинг

--- lesson_13.js
+++ lesson_13.js
@@ -1,6 +1,6 @@
 let gameRuleset = {
-    diceNumber: 2,
-    maxAttempts: 3
+    diceNumber: 1,
+    maxAttempts: 1
 };
 
 let firstCat = {
@@ -13,7 +13,12 @@ let secondCat = {
     points: 0
 };
 
-let cats = [firstCat, secondCat];
+let thirdCat = {
+    name: 'Рокки',
+    points: 0
+};
+
+let cats = [firstCat, secondCat, thirdCat];
 
 let runGame = function (ruleset, players) {
     for (let currentAttempt = 1; currentAttempt <= ruleset.maxAttempts; currentAttempt++) {
@@ -22,22 +27,53 @@ let runGame = function (ruleset, players) {
             players[i].points += throwResult;
             console.log(players[i].name + ' выбросил ' + players[i].points);
         }
-
     }
     return players;
 };
 
 let getWinners = function (players) {
     let winners = [];
+    let max = players[0];
+
     for (let i = 0; i < players.length; i++) {
         let currentPlayer = players[i];
-        console.log(currentPlayer);
-    };
+        if (currentPlayer.points > max.points) {
+            max = currentPlayer;
+            winners = [max];
+        } else {
+            if (currentPlayer.points === max.points) {
+                winners.push(currentPlayer);
+            }
+        }
+    }
+
     return winners;
-}
+};
+
+
+let printWinners = function (players, winners) {
+    if (players.length === winners.length) {
+        console.log('Все коты как на подбор!');
+        return;
+    }
+
+    let message = 'Победил ';
+    if (winners.length > 1) {
+        message = 'Победили ';
+    }
+
+    for (let i = 0; i < winners.length; i++) {
+        if (i >= 1) {
+            message += ', ';
+        }
+        message += winners[i].name;
+    }
+
+    message += ' с количеством очков: ' + winners[0].points;
+
+    console.log(message);
+};
 
 cats = runGame(gameRuleset, cats);
-console.log(cats);
-
 let tops = getWinners(cats);
-console.log(tops);
+printWinners(cats, tops);
