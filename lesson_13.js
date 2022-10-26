98a38006ba2449e7b05d103b470d8b753dfb0ce5
rocetDim <dmitriydem81@mail.ru>
2022-10-26 21:15:49 +0300

    Игра в кости

---
+++ lesson_13.js
@@ -0,0 +1,31 @@
+let gameRuleset = {
+    diceNumber: 2,
+    maxAttempts: 3
+};
+
+let firstCat = {
+    name: 'Кекс',
+    points: 0
+};
+
+let secondCat = {
+    name: 'Рудольф',
+    points: 0
+};
+
+let cats = [firstCat, secondCat];
+
+let runGame = function (ruleset, players) {
+    for (let currentAttempt = 1; currentAttempt <= ruleset.maxAttempts; currentAttempt++) {
+        for (let i = 0; i < players.length; i++) {
+            let throwResult = keks.throwDice(ruleset.diceNumber, ruleset.diceNumber * 6);
+            players[i].points += throwResult;
+            console.log(players[i].name + ' выбросил ' + players[i].points);
+        }
+
+    }
+    return players;
+};
+
+cats = runGame(gameRuleset, cats);
+console.log(cats);
