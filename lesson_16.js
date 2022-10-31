27558bd065b42f155b2634e265aeb7a6d8cb5e70
rocetDim <dmitriydem81@mail.ru>
2022-10-31 22:11:31 +0300

    Прогрессия

---
+++ lesson_16.js
@@ -0,0 +1,9 @@
+//Геометрическая прогрессия
+let startNumber = 1;
+let multiplier = 4;
+let quantity = 7;
+
+for (let i = 0; i < quantity; i++) {
+    console.log(startNumber);
+    startNumber *= multiplier;
+}
